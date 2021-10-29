1cb65e250317d3f1852b08c9d345f799c81de4a3
onairsparks <onairsparks@gmail.com>
2021-10-29 13:06:36 +0900

    jpa study base setting

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -1,13 +1,16 @@
-package stury.datajpa.controller;
+package study.datajpa.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
+        log.info("controller enter");
         return "hello";
     }
 }
